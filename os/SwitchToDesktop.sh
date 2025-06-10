a89744423c415569b1f172ebfcf1ed8ffcbbbd3a
Ronald Sardarian <ronaldsardarian@gmail.com>
2025-06-10 16:46:56 -0700

    Export 2025-06-10 16:46:55

---
+++ os/SwitchToDesktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+    #sudo raspi-config nonint do_boot_behaviour B4 #for desktop logged in
+    #sudo raspi-config nonint do_boot_behaviour B2 #for CLI logged in
+
+    sudo raspi-config nonint do_boot_behaviour B4
+    sudo reboot now
